9d9b125d338ff8b7412ecbfaf7b6329ad52e638d
Sean Murphy <sean@iamseanmurphy.com>
2024-09-22 20:24:27 +0000

    8.5.5

---
+++ wpvr/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpvr/wpvr',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'cd090a752ab4eddda1aa37c25dd349cc51702f33',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpvr/wpvr' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'cd090a752ab4eddda1aa37c25dd349cc51702f33',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
